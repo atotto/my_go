0cd4021290aba8d693c51cae4542314d4868d6d0
Ato ARAKI <ato.araki@gmail.com>
2013-08-18 11:27:23 +0900

    Implement test and benchmark

--- packageExample/my/my_test.go
+++ packageExample/my/my_test.go
@@ -1,15 +1,21 @@
 package my_test
 
 import (
+	. "."
 	"testing"
 )
 
 func TestMyFuncA(t *testing.T) {
+	expected := 123
+	actual := FuncA()
 
+	if expected != actual {
+		t.Errorf("want %d, got %d", expected, actual)
+	}
 }
 
 func BenchmarkMyFuncA(b *testing.B) {
 	for i := 0; i < b.N; i++ {
-
+		FuncA()
 	}
 }
